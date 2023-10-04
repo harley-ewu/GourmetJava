c51770e9226f4b9fa2236ef7015d2d2eac1811fa
Timothy N <tnelson32@ewu.edu>
2023-10-04 13:12:38 -0700

    Tim commit 1
    Initial done with ClassBox

--- ClassBox.java
+++ ClassBox.java
@@ -23,19 +23,52 @@ public class ClassBox {
     }
 
     public void deleteAttribute(String name){
-        attributes.remove();
+        Attribute a = null;
+        for (Attribute attribute : this.attributes) {
+            if (attribute.getName().equals(name)) {
+                a = attribute;
+            }
+        }
+        if(a==null){
+            System.out.println("Error: Attribute not found");
+        }
+        else {
+            this.attributes.remove(a);
+        }
     }
 
-    public void renameAttribute(String name){
-
+    public void renameAttribute(String att, String newName){
+        Attribute a = null;
+        for (Attribute attribute : this.attributes) {
+            if (attribute.getName().equals(att)) {
+                a = attribute;
+            }
+        }
+        if(a==null){
+            System.out.println("Error: Attribute not found");
+        }
+        else {
+            a.setName(name);
+        }
     }
 
-    public void addRelationship(String name, String class1, String class2, String type){
-
+    public void addRelationship(String name, ClassBox class1, ClassBox class2, String type, String fromNum, String toNum){
+        this.relationships.add(new Relationship(name, class1, class2, type, fromNum, toNum));
     }
 
     public void deleteRelationship(String name){
-
+        Relationship r = null;
+        for (Relationship relationship : this.relationships) {
+            if (relationship.getName().equals(name)) {
+                r = relationship;
+            }
+        }
+        if(r==null){
+            System.out.println("Error, relationship not found");
+        }
+        else{
+            this.relationships.remove(r);
+        }
     }
 
     public void renameClass(String name){
@@ -43,7 +76,16 @@ public class ClassBox {
     }
     //For list class details
     public String toString(){
-
+        System.out.println("Class: "+this.name);
+        System.out.println("Type: "+this.type);
+        System.out.println("Attributes: ");
+        for(Attribute a: this.attributes){
+            System.out.println("\t"+a);
+        }
+        System.out.println("Relationships: ");
+        for(Relationship r: this.relationships){
+            System.out.println("\t"+r);
+        }
     }
 
     public String getName() {
