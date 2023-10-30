20ee432a6d0206f43eb66269362dcb76ec815d19
Ben Knoll <118641351+BenKnoll3@users.noreply.github.com>
2023-10-30 12:54:23 -0700

    Created ModelDiagram
    - I created the class that will be our entry point to the model.

---
+++ src/main/java/ModelDiagram.java
@@ -0,0 +1,24 @@
+package src.main.java;
+
+// This is the class representing all data within the model
+public class ModelDiagram {
+    // Created classes will now be stored here
+    // Methods for manipulating the model will be stored here
+    // Also needs some sort of way to push data to the view so it is able to display it
+
+    /*Methods needed to be implemented
+        - addClass
+        - deleteClass
+        - renameClass
+        - addRelationship
+        - deleteRelationship
+        - addAttribute
+        - deleteAttribute
+        - renameAttribute
+        - Save
+        - Load
+
+        Not sure about implementation yet:
+        - Some sort of method to update to view
+     */
+}
