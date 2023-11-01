0817e4638f76ad9164e748dd3b36ae734c113de3
David Parker <dparker16@ewu.edu>
2023-11-01 12:20:39 -0700

    OK
    It builds and the stubs are scuffed

--- src/main/java/CLI.java
+++ src/main/java/CLI.java
@@ -6,7 +6,162 @@ import java.util.Scanner;
 
 public class CLI {
     //Scanner to read user input
-    Scanner kb = new Scanner(System.in);
+    private static Scanner kb = new Scanner(System.in);
+
+    public static void menu() {
+        boolean cont = true;
+        while (cont) {
+            Controller.printMenu();
+            int input2;
+            System.out.print("Choice:");
+            // get user input of 1-15
+            // call io method below
+            // io method calls actual method in other classes
+            int input = Integer.parseInt(kb.nextLine());
+            switch (input) {
+                case 1:
+                    if(false){
+                        System.out.println("Nothing to display! Please make a class first");
+                    } else {
+                        System.out.println("Please choose a number from the options below: ");
+                        System.out.println("1.) Display Classes");
+                        System.out.println("2.) Display Classes Detailed");
+                        System.out.println("3.) Display Class Details");
+                        System.out.println("4.) Display Relationships");
+                        System.out.println("5.) Help");
+                        System.out.println("6.) Back");
+                        System.out.print("Choice:");
+                        input2 = Integer.parseInt(kb.nextLine());
+                        if (input2 == 1) {
+                            Controller.listClasses();
+                        } else if (input2 == 2) {
+                            Controller.listDetailedClasses();
+                        } else if (input2 == 3) {
+                            Controller.listClass("Placeholder");
+                        } else if (input2 == 4) {
+                            Controller.listRelationships();
+                        } else if (input2 == 5) {
+                            Controller.listHelp();
+                        } else if (input2 == 6) {
+                            return;
+                        } else {
+                            System.out.println("Invalid input, please try again");
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("Please choose a number from the options below: ");
+                    System.out.println("1.) Add Class");
+                    System.out.println("2.) Remove Class");
+                    System.out.println("3.) Rename Class");
+                    System.out.println("4.) Help");
+                    System.out.println("5.) Back");
+                    System.out.print("Choice:");
+                    input2 = Integer.parseInt(kb.nextLine());
+                    if (input2 == 1) {
+                        Controller.addClass("test","test2");
+                    } else if (input2 == 2) {
+                        Controller.deleteClass("name");
+                    } else if (input2 == 3) {
+                        //renameClass();
+                    } else if (input2 == 4) {
+                        Controller.classHelp();
+                    } else if (input2 == 5) {
+                        return;
+                    } else {
+                        System.out.println("Invalid input, please try again");
+                    }
+                    break;
+                case 3:
+                    //if (createdClasses.isEmpty()) {
+                    if(false){
+                        System.out.println("Please create a class first");
+                    } else {
+                        System.out.println("Please choose a number from the options below: ");
+                        System.out.println("1.) Add Attribute");
+                        System.out.println("2.) Remove Attribute");
+                        System.out.println("3.) Rename Attribute");
+                        System.out.println("4.) Help");
+                        System.out.println("5.) Back");
+                        System.out.print("Choice:");
+                        input2 = Integer.parseInt(kb.nextLine());
+                        if (input2 == 1) {
+                            Controller.addAttribute();
+                        } else if (input2 == 2) {
+                            Controller.deleteAttribute();
+                        } else if (input2 == 3) {
+                            Controller.renameAttribute();
+                        } else if (input2 == 4) {
+                            Controller.attributeHelp();
+                        } else if (input2 == 5) {
+                            return;
+                        } else {
+                            System.out.println("Invalid input, please try again");
+                        }
+                    }
+                    break;
+                case 4:
+                    //if (createdClasses.size() < 2) {
+                    if(false){
+                        System.out.println("Please create 2 classes first");
+                    } else {
+                        System.out.println("Please choose a number from the options below: ");
+                        System.out.println("1.) Add Relationship");
+                        System.out.println("2.) Remove Relationship");
+                        System.out.println("3.) Help");
+                        System.out.println("4.) Back");
+                        System.out.print("Choice:");
+                        input2 = Integer.parseInt(kb.nextLine());
+                        if (input2 == 1) {
+                            //addRelationship();
+                        } else if (input2 == 2) {
+                            //deleteRelationship();
+                        } else if (input2 == 3) {
+                            Controller.relationshipHelp();
+                        } else if (input2 == 4) {
+                            return;
+                        } else {
+                            System.out.println("Invalid input, please try again");
+                        }
+                    }
+                    break;
+                case 5:
+                    System.out.println("Please choose a number from the options below: ");
+                    System.out.println("1.) Save");
+                    System.out.println("2.) Load");
+                    System.out.println("3.) Help");
+                    System.out.println("4.) Back");
+                    System.out.print("Choice:");
+                    input2 = Integer.parseInt(kb.nextLine());
+                    if (input2 == 1) {
+                        ModelDiagram.save();
+                    } else if (input2 == 2) {
+                        ModelDiagram.load();
+                    } else if (input2 == 3) {
+                        Controller.saveLoadHelp();
+                    } else if (input2 == 4) {
+                        return;
+                    } else {
+                        System.out.println("Invalid input, please try again");
+                    }
+                    break;
+                case 6:
+                    Controller.help();
+                    break;
+                case 7:
+                    System.out.println("Are you sure you want to exit? Type \"yes\" to confirm");
+                    System.out.print("yes/no:");
+                    if (kb.nextLine().equalsIgnoreCase("yes")) {
+                        System.out.println("Program Closed! Bye!");
+                        cont = false;
+                    }
+                    break;
+                default:
+                    System.out.println("That is not a valid input. Please try again");
+                    break;
+            }
+        }
+    }
 
     // This method will take class name input from the user in CLI mode and send it
     // to the controller
@@ -37,7 +192,7 @@ public class CLI {
         System.out.println("What index do you want to remove?");
         Controller.listClasses();
         System.out.print("Class Index:");
-        int input = Integer.parseInt(kb.nextLine());
+        String input = kb.nextLine();
 
         //Test to see if deleting was sucessful
         if (Controller.deleteClass(input)) {
@@ -55,7 +210,7 @@ public class CLI {
         System.out.println("What index do you want to rename?");
         Controller.listClasses();
         System.out.print("Class Index:");
-        int num = Integer.parseInt(kb.nextLine());
+        String num = kb.nextLine();
 
         System.out.println("What would you like to rename your class?");
         System.out.print("New Class Name:");
@@ -89,7 +244,8 @@ public class CLI {
         int type = Integer.parseInt(kb.nextLine());
 
         //Test to see if adding relationship was sucessful
-        if (Controller.addRelationship(index1, index2, type)) {
+        //if (Controller.addRelationship(index1, index2, type)) {
+        if(false){
             System.out.println("Relationship created!");
         } else {
             System.out.println("Bad inputs for relationship, creation cancelled");
@@ -109,7 +265,8 @@ public class CLI {
         String answer = kb.nextLine();
 
         //Test to see if deleting relationship was sucessful
-        if (Controller.deleteRelationship(index1, index2, answer)) {
+        //if (Controller.deleteRelationship(index1, index2, answer)) {
+        if(false){
             System.out.println("Relationship created!");
         } else {
             System.out.println("Bad inputs for relationship, deletion cancelled.");
