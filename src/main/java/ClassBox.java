9ccc669a1828673531f4c1bdf9af16019fcd70a7
David Parker <dparker16@ewu.edu>
2023-11-02 12:41:39 -0700

    Added comments and tweaks
    - printClassTypes() is now listClassTypes() and returns a list of the possible ClassTypes instead of printing them out (for consistency and compatability)
    - removed unnecessary return type of method in ClassBox
    - changed a few variable names to make it clearer
    - changed ClassBox toString() to return "not implemented" instead of null
    - added comments to several methods
    - add/deleteRelationship() now checks that the Relationship exists before deleting them - this can be optimized but it should work for now, optimizations can happen later

--- src/main/java/ClassBox.java
+++ src/main/java/ClassBox.java
@@ -1,5 +1,6 @@
 package src.main.java;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class ClassBox {
@@ -39,11 +40,10 @@ public class ClassBox {
         this.type = ClassBox.ClassType.values()[type - 1];
     }
 
-    public static void printClassTypes() {
-        ClassBox.ClassType[] types = ClassBox.ClassType.values();
-        for (int i = 0; i < 5; ++i) {
-            System.out.println((i + 1) + " - " + types[i].name());
-        }
+    //returns an array that contains the names of all constants in the ClassType enum
+    //I shamelessly found the code online -David
+    public static String[] listClassTypes() {
+        return Arrays.stream(ClassType.values()).map(Enum::name).toArray(String[]::new);
     }
 
     public void addMethod(String name, Visibility view, String type, LinkedList<String> params){
@@ -57,6 +57,7 @@ public class ClassBox {
         Field newField = new Field(name, view, type);
         this.fields.add(newField);
     }
+
 
     public void deleteAttribute(Attribute a) {
         if (a.getClass().getSimpleName().equals("Method")) {
@@ -113,6 +114,7 @@ public class ClassBox {
     }
 
 
+    //finds a relationship between two ClassBoxes if it exists, or null if the relationship does not exist
     public static Relationship findRelationship(final ClassBox cb1, final ClassBox cb2) {
         for (Relationship rel : cb1.parents) {
             if (rel.getOtherClass().equals(cb2)) {
@@ -140,42 +142,39 @@ public class ClassBox {
         childClass.parents.add(new Relationship(parentClass,type));
     }
 
-    //returns true only if a Relationship was deleted
-    //returns false if no Relationship was deleted or it did not exist
-    public static boolean deleteRelationship(final ClassBox cb1, final ClassBox cb2) {
+   //Deletes the relationship between the two ClassBox objects
+    public static void deleteRelationship(final ClassBox cb1, final ClassBox cb2) {
         for (Relationship rel : cb1.parents) {
             if (rel.getOtherClass().equals(cb2)) {
                 cb1.parents.remove(rel);
                 cb2.children.remove(rel);
-                return true;
             }
         }
         for (Relationship rel : cb1.children) {
             if (rel.getOtherClass().equals(cb2)) {
                 cb1.children.remove(rel);
                 cb2.parents.remove(rel);
-                return true;
             }
         }
-        return false;
     }
 
+    //returns a list of ONLY the class names in the calling ClassBox's parents list
     public String[] listRelationships() {
-        String[] rels = new String[this.parents.size()];
-        for (int i = 0; i < rels.length; ++i) {
-            rels[i] = this.parents.get(i).toString();
+        String[] relationships = new String[this.parents.size()];
+        for (int i = 0; i < relationships.length; ++i) {
+            relationships[i] = this.parents.get(i).toString();
         }
-        return rels;
+        return relationships;
     }
 
     public void rename(final String name) {
         this.name = name;
     }
-    //For list class details
+
 
     @Override
     public String toString() {
-        return null;
+        return "ClassBox toString() not implemented";
     }
 
 
--- src/main/java/ModelDiagram.java
+++ src/main/java/ModelDiagram.java
@@ -174,7 +174,9 @@ public class ModelDiagram {
         if (parent == null || child == null)
             return false;
 
-        //check if relationship between boxes exist within ClassBox
+        Relationship relationship = ClassBox.findRelationship(parent, child);
+        if(relationship != null)
+            return false;
 
         ClassBox.addRelationship(parent, child, type);
         return true;
@@ -191,10 +193,16 @@ public class ModelDiagram {
         if (parent == null || child == null)
             return false;
 
-        return ClassBox.deleteRelationship(parent, child);
+        Relationship relationship = ClassBox.findRelationship(parent, child);
+        if(relationship == null)
+            return false;
 
+        ClassBox.deleteRelationship(parent, child);
+        return true;
     }
 
+    //returns list of names of ONLY each createdClasses's parent classes
+    //each createdClass's list is stored in a list (list of lists)
     public static String[][] listRelationships() {
         String[][] list = new String[createdClasses.size()][];
         for(int i = 0; i < createdClasses.size(); ++i){
@@ -203,7 +211,17 @@ public class ModelDiagram {
         return list;
     }
 
-    //How do we want to handle the class details?
+    /*We need to return:
+        Class name
+            - Class type
+        Its methods
+            - method return types and parameter list
+            - visibility
+        Its fields
+            - the field data types and visibility
+
+        I'm not sure how to handle this -David
+     */
     public static String[] listClassDetails(final String name) {
         if(name == null || name.isEmpty())
             throw new IllegalArgumentException("bad param passed to ModelDiagram.listClass");
