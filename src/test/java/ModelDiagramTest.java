de9a08a0237fd800e1901f1bdd53c13a2afb3bfa
LichDaddy <msinclair@ewu.edu>
2023-11-01 19:49:02 -0700

    Made a JUnit test and added required packages

---
+++ src/test/java/ModelDiagramTest.java
@@ -0,0 +1,33 @@
+package src.test.java;
+
+import org.junit.Test;
+import src.main.java.ModelDiagram;
+import static org.junit.Assert.*;
+
+public class ModelDiagramTest {
+
+    @Test
+    public void testAddClass() {
+        // Test adding a new class
+        assertTrue(ModelDiagram.addClass("TestClass", 1));
+
+        // Test adding a class with the same name
+        assertFalse(ModelDiagram.addClass("TestClass", 2));
+
+        // Test adding a class with a null name
+        try {
+            ModelDiagram.addClass(null, 1);
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException exception) {
+            assertEquals("bad param passed to ModelDiagram.addClass", exception.getMessage());
+        }
+
+        // Test adding a class with an empty name
+        try {
+            ModelDiagram.addClass("", 1);
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException exception) {
+            assertEquals("bad param passed to ModelDiagram.addClass", exception.getMessage());
+        }
+    }
+}
