49d057135008cf8b48b623c0da09acb616313081
LichDaddy <msinclair@ewu.edu>
2023-11-05 11:48:57 -0800

    Changed the constructor for JUnit RelationshipTest

--- src/test/java/RelationshipBuilderTest.java
+++ src/test/java/RelationshipBuilderTest.java
@@ -8,10 +8,11 @@ public class RelationshipBuilderTest {
 
     @Test
     public void testConstructor() {
-        RelationshipBuilder builder = new RelationshipBuilder(9, 6, 3);
+        RelationshipBuilder builder = new RelationshipBuilder(9, 6, 3, "parent");
         assertEquals(9, builder.getFirstIndex());
         assertEquals(6, builder.getSecondIndex());
         assertEquals(3, builder.getRelationshipType());
+        assertEquals("parent", builder.getRelationshipType());        
     }
     
 }
