a4e7b85230361618d0a5dbd3dd82d3c7db336a1c
Ben Knoll <bknoll@ewu.edu>
2023-11-26 17:48:54 -0800

    Some tests

--- src/test/j/ClassBoxTest.java
+++ src/test/j/ClassBoxTest.java
@@ -76,4 +76,50 @@ public class ClassBoxTest {
         // THIS NEEDS TO BE FIXED // It should be [-testMethod(renamedParam) : int] as output
         assertEquals("[-testMethod(renamedParam) : int]", classBox.getMethods().toString());
     }
+
+    @Test
+    public void testGetType() {
+        ClassBox classBox = new ClassBox("Test", 1);
+        assertEquals("CLASS", classBox.getType());
+    }
+
+    @Test
+    public void testClone() {
+        ClassBox classBox = new ClassBox("Test", 1);
+        ClassBox clonedBox = classBox.clone();
+        assertEquals(clonedBox.getType(), classBox.getType());
+        assertEquals(clonedBox.getName(), classBox.getName());
+    }
+
+    @Test
+    public void testDeleteMethod() {
+        LinkedList<String> testParams = new LinkedList<String>();
+        testParams.add("testParam1");
+
+        // Make a new classBox
+        ClassBox classBox = new ClassBox("Test", 1);
+        // Add a method
+        classBox.addMethod("testMethod", 1,"int", testParams);
+        classBox.deleteMethod("testMethod");
+        assertEquals(classBox.getMethods().size(), 0);
+    }
+
+    @Test
+    public void testDeleteParam() {
+        LinkedList<String> testParams = new LinkedList<String>();
+        testParams.add("testParam");
+
+        // Make a new classBox
+        ClassBox classBox = new ClassBox("Test", 1);
+        // Add a method
+        classBox.addMethod("testMethod", 1,"int", testParams);
+
+        classBox.deleteParam("testMethod", "testParam");
+        assertEquals("[-testMethod() : int]", classBox.getMethods().toString());
+    }
+
+    @Test
+    public void renameMethodTest() {
+
+    }
 }
